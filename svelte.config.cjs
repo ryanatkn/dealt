0a308ce67bd9ad3f4c97fd4b3b2caf9d65d6109c
Ryan Atkinson <mail@ryanatkn.com>
2021-04-14 16:43:39 -0600

    remove package namespacing for production

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -2,25 +2,13 @@ const {typescript} = require('svelte-preprocess-esbuild');
 const staticAdapter = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
-const dev = process.env.NODE_ENV !== 'production';
-
-// TODO import from gro
-const toPackageRepoName = (pkg) =>
-	pkg.name.includes('/') ? pkg.name.split('/').slice(1).join('/') : pkg.name;
-const toSvelteKitBasePath = (pkg, dev) => (dev ? '' : `/${toPackageRepoName(pkg)}`);
-
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	preprocess: typescript(),
 	kit: {
 		adapter: staticAdapter(),
-
 		appDir: 'app', // because _app is ignored by GitHub pages by default
-		paths: {base: toSvelteKitBasePath(pkg, dev)},
-
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-
 		vite: {
 			ssr: {
 				noExternal: Object.keys(pkg.dependencies || {}),
