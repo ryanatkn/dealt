31506fe2b40878ee51c06383167f21e0059b7323
Ryan Atkinson <mail@ryanatkn.com>
2021-04-23 16:24:00 -0600

    improve credits

--- src/lib/AboutLink.svelte
+++ src/lib/AboutLink.svelte
@@ -3,11 +3,19 @@
 </script>
 
 <div class="about-link">
+	<h1>public domain <a href="https://github.com/feltcoop/dealt/blob/main/license">🐦</a></h1>
 	<Thumbnail href="https://github.com/ryanatkn/dealt"><h2>source code and credits</h2></Thumbnail>
 </div>
 
 <style>
 	.about-link {
 		text-align: center;
+	}
+	h1 {
+		margin: 0;
+		font-weight: 300;
+	}
+	h2 {
+		margin: 0;
 	}
 </style>
