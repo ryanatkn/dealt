01ff22d432bb0ca5b9caf97932197dfc71496125
Ryan Atkinson <mail@ryanatkn.com>
2021-07-13 12:30:03 -0500

    fixes

--- src/lib/random.ts
+++ src/lib/random.ts
@@ -2,56 +2,49 @@ import {browser} from '$app/env';
 
 // TODO upstream to felt -- implements deterministic random numbers for SSR
 
-export interface To_Random_Number {
+interface To_Random_Float {
 	(): number;
 }
-export interface To_To_Random_Number {
-	(i?: number): To_Random_Number;
-}
 
+export const to_random_float: To_Random_Float = () => Math.random();
+
+// TODO find a small seeded prng library, because this returns very uniform values
 export const VARY_RANDOM = 0.42;
-
-export const to_random_float: To_Random_Number = () => Math.random();
-// TODO get a good small seeded implementation or something
-export const to_to_deterministic_random_float: To_To_Random_Number =
+export const to_to_deterministic_random_float: (i?: number) => To_Random_Float =
 	(i = 0) =>
 	() =>
-		VARY_RANDOM + i++ / 10000000000;
+		VARY_RANDOM + i++ / 100000000;
 
-export const random_float: To_Random_Number = browser
+export const random_float: To_Random_Float = browser
 	? to_random_float
 	: to_to_deterministic_random_float();
 
-// mutates `array` - clone first for immutability
-export const shuffle: <T>(array: T[], random?: To_Random_Number) => T[] = (
-	array,
-	random = random_float,
-) => {
-	const len = array.length;
-	const max = len - 1;
-	for (let i = 0; i < len; i++) {
-		const dest_index = random_int(0, max, random);
-		if (i === dest_index) continue;
-		const destItem = array[dest_index];
-		array[dest_index] = array[i];
-		array[i] = destItem;
-	}
-	return array;
-};
-
-export const random_bool: (random?: To_Random_Number) => boolean = (random = random_float) =>
-	random() > 0.5;
-
-export const random_int: (min: number, max: number, random?: To_Random_Number) => number = (
+export const to_random_int: (min: number, max: number, random?: To_Random_Float) => number = (
 	min,
 	max,
 	random = random_float,
 ) => Math.floor(random() * (max - min + 1)) + min;
 
-export const random_item: <T>(arr: T[], random?: To_Random_Number) => T = (
-	arr,
-	random = random_float,
-) => arr[random_int(0, arr.length - 1, random)];
+export const to_to_deterministic_random_int: (i?: number) => (min: number, max: number) => number =
+	(i = 0) =>
+	(min, max) => {
+		let int = min + i++;
+		if (int <= max) return int;
+		const diff = max - min;
+		while (int > max) {
+			int -= diff;
+		}
+		return int;
+	};
+
+interface To_Random_Int {
+	(min: number, max: number): number;
+}
+
+export const random_int: To_Random_Int = browser ? to_random_int : to_to_deterministic_random_int();
+
+export const random_item: <T>(arr: T[], random?: To_Random_Int) => T = (arr, random = random_int) =>
+	arr[random(0, arr.length - 1)];
 
 // TODO might need another version that doesn't use a `Set` -- `random_items_with_duplicates`
 export const random_items = <T>(items: T[], count: number, random = random_item) => {
@@ -62,3 +55,23 @@ export const random_items = <T>(items: T[], count: number, random = random_item)
 	}
 	return Array.from(results);
 };
+
+// mutates `array` - clone first for immutability
+export const shuffle: <T>(array: T[], random?: To_Random_Int) => T[] = (
+	array,
+	random = random_int,
+) => {
+	const len = array.length;
+	const max = len - 1;
+	for (let i = 0; i < len; i++) {
+		const dest_index = random(0, max);
+		if (i === dest_index) continue;
+		const destItem = array[dest_index];
+		array[dest_index] = array[i];
+		array[i] = destItem;
+	}
+	return array;
+};
+
+export const random_bool: (random?: To_Random_Float) => boolean = (random = random_float) =>
+	random() > 0.5;
